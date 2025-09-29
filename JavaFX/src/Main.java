1ae64325131b33df62951a3e43f6fb9aff6b2ed7
yonatanzxc123 <168387612+yonatanzxc123@users.noreply.github.com>
2025-09-29 14:53:13 +0300

    Fixed cycles calculations

--- JavaFX/src/Main.java
+++ JavaFX/src/Main.java
@@ -2,7 +2,7 @@ import ui.MainScreen;
 
 import static javafx.application.Application.launch;
 
-public class Main {
+public class  Main {
     public static void main(String[] args) {
        MainScreen mainScreen = new MainScreen();
        mainScreen.launch(mainScreen.getClass(),args);
